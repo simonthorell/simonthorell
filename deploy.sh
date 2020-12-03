5155e54dd57bd11b4a3d27540c87c09acc1cbdb1
Simon Thorell <kontak@simonthorell.se>
2020-12-04 00:27:29 +0100

    Change to deploy ssh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 git push -f git@github.com:simonthorell/simonthorell.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:simonthorell/simonthorell.git master:gh-pages
+git push -f git@github.com:simonthorell/simonthorell.git master:gh-pages
 
 cd -
